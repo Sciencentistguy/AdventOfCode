0882399c456842d7fbea1c3d7a9b56da5007cc62
Sciencentistguy <jamie.e.quigley@gmail.com>
2019-12-07 15:34:12 +0000

    Re-did task 2 with task 5's structure

--- Task 2/task_1.cpp
+++ Task 2/task_1.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include "cpu.h"
 
 std::string getInput() {
     std::string str{};
@@ -22,34 +23,9 @@ std::string getInput() {
 }
 
 int main(int argc, char* argv[]) {
-    std::vector<std::string> inputVector;
-    std::vector<uint32_t> memory;
-    std::string input = getInput();
-    std::istringstream ss(input);
-    std::string token;
-
-    while (std::getline(ss, token, ',')) {
-        inputVector.push_back(token);
-    }
-
-    for (auto s : inputVector) {
-        memory.push_back(std::stoi(s));
-    }
-
-    memory[1] = 12;
-    memory[2] = 02;
-
-    for (int pos = 0; memory[pos] != 99; pos += 4) {
-        switch ((memory[pos])) {
-            case 1:
-                memory[memory[pos + 3]] = memory[memory[pos + 1]] + memory[memory[pos + 2]];
-                break;
-
-            case 2:
-                memory[memory[pos + 3]] = memory[memory[pos + 1]] * memory[memory[pos + 2]];
-                break;
-        }
-    }
-
-    std::cout << memory[0] << std::endl;
+    CPU cpu{getInput()};
+    cpu.mem[1] = 12;
+    cpu.mem[2] = 02;
+    auto mem = cpu.run();
+    std::cout << mem[0] << std::endl;
 }
--- Task 2/task_2.cpp
+++ Task 2/task_2.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include "cpu.h"
 
 std::string getInput() {
     std::string str{};
@@ -22,48 +23,15 @@ std::string getInput() {
 }
 
 int main(int argc, char* argv[]) {
-    std::vector<std::string> inputVector;
-    std::vector<uint32_t> memory;
-    std::string input = getInput();
-    std::istringstream ss(input);
-    std::string token;
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            CPU cpu{getInput()};
+            cpu.mem[1] = i;
+            cpu.mem[2] = j;
+            auto mem = cpu.run();
 
-    while (std::getline(ss, token, ',')) {
-        inputVector.push_back(token);
-    }
-
-    for (auto s : inputVector) {
-        memory.push_back(std::stoi(s));
-    }
-
-    int out;
-    std::vector<uint32_t> aux{memory};
-
-    for (int pos1 = 0; pos1 < 100; pos1++) {
-        for (int pos2 = 0; pos2 < 100; pos2++) {
-            memory = aux;
-            memory[1] = pos1;
-            memory[2] = pos2;
-
-            for (int currPos = 0; memory[currPos] != 99; currPos += 4) {
-                switch ((memory[currPos])) {
-                    case 1:
-                        memory[memory[currPos + 3]] = memory[memory[currPos + 1]] + memory[memory[currPos + 2]];
-                        break;
-
-                    case 2:
-                        memory[memory[currPos + 3]] = memory[memory[currPos + 1]] * memory[memory[currPos + 2]];
-                        break;
-                }
-            }
-
-            if (memory[0] == 19690720) {
-                std::cout << 100 * pos1 + pos2 << std::endl;
-                pos1 = 100;
-                pos2 = 100;
-            }
+            if (mem[0] == 19690720)
+                printf("%02d%02d\n", mem[1], mem[2]);
         }
     }
-
-    std::cout << out << std::endl;
 }
