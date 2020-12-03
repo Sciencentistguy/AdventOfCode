442569a89e88434030c31375de28949fc8838d94
Jamie Quigley <jamie@quigley.xyz>
2020-12-03 11:03:15 +0000

    Maybe a tiny bit faster on day three in c++

--- 2020/cpp/day_three.h
+++ 2020/cpp/day_three.h
@@ -19,18 +19,18 @@ struct day_three {
     std::vector<std::string> input;
 
     day_three() : input{readFile("Inputs/day_three.txt")} {
-        fmt::print("Parsing input for day two took 0ns\n");
+        fmt::print("Parsing input for day three took 0ns\n");
     }
 
-    inline bool isTree(int x, int y) const {
+    inline bool isTree(unsigned int x, unsigned int y) const {
         const auto width = input[0].length();
         return input[y][x % width] == '#';
     }
 
-    inline int getTreesSlope(int x_step, int y_step) const {
-        int x{};
-        int y{};
-        int count{};
+    inline unsigned int getTreesSlope(unsigned int x_step, unsigned int y_step) const {
+        unsigned int x{};
+        unsigned int y{};
+        unsigned int count{};
         const auto y_limit = input.size() - y_step;
         do {
             count += isTree(x, y);
@@ -42,7 +42,7 @@ struct day_three {
 
     void part_one() const {
         auto start = std::chrono::high_resolution_clock::now();
-        auto count = getTreesSlope(3, 1);
+        auto count = getTreesSlope(3u, 1u);
         auto end = std::chrono::high_resolution_clock::now();
         fmt::print("The answer for day three part one is {}\n", count);
         fmt::print("Took {}ns\n", std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
@@ -50,11 +50,11 @@ struct day_three {
 
     void part_two() const {
         auto start = std::chrono::high_resolution_clock::now();
-        long count = getTreesSlope(1, 1);
-        count *= getTreesSlope(3, 1);
-        count *= getTreesSlope(5, 1);
-        count *= getTreesSlope(7, 1);
-        count *= getTreesSlope(1, 2);
+        long count = getTreesSlope(1u, 1u);
+        count *= getTreesSlope(3u, 1u);
+        count *= getTreesSlope(5u, 1u);
+        count *= getTreesSlope(7u, 1u);
+        count *= getTreesSlope(1u, 2u);
         auto end = std::chrono::high_resolution_clock::now();
         fmt::print("The answer for day three part one is {}\n", count);
         fmt::print("Took {}ns\n", std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
