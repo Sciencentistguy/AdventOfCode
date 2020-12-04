53b14d42b181f28d1961fcf4d5eea8ab2c99b501
Jamie Quigley <jamie@quigley.xyz>
2020-12-04 14:35:17 +0000

    Massive optimisation, std::unordered_map bad ha

--- 2020/cpp/day_four.h
+++ 2020/cpp/day_four.h
@@ -7,7 +7,6 @@
 #include <stdexcept>
 #include <string>
 #include <string_view>
-#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -17,7 +16,22 @@
 #include "common.h"
 
 struct day_four {
-    using passport_t = std::unordered_map<std::string_view, std::string_view>;
+    struct passport_t {
+        int birth_year{};
+        int issue_year{};
+        int expiration_year{};
+        std::string_view height_unit{};
+        int height{};
+        std::string_view hair_colour{};
+        std::string_view eye_colour{};
+        std::string_view passport_id{};
+
+        bool hasAllFields() const {
+            return birth_year && issue_year && expiration_year && height && !height_unit.empty() && !hair_colour.empty() && !eye_colour.empty()
+                   && !passport_id.empty();
+        }
+    };
+
     std::vector<std::string> input_strings;
     std::vector<passport_t> input;
 
@@ -32,12 +46,28 @@ struct day_four {
             }
             while (*current_line != "") {
                 auto words = split(*current_line, ' ');
-                for (const auto& sv : words) {
-                    auto t = split(sv, ':');
-                    // if (t.size() != 2) {
-                    // throw std::runtime_error("Invalid input");
-                    //}
-                    passport.emplace(t[0], t[1]);
+                for (const auto& pair : words) {
+                    auto t = split(pair, ':');
+                    if (t[0] == "byr") {
+                        passport.birth_year = std::atoi(t[1].data());
+                    } else if (t[0] == "iyr") {
+                        passport.issue_year = std::atoi(t[1].data());
+                    } else if (t[0] == "eyr") {
+                        passport.expiration_year = std::atoi(t[1].data());
+                    } else if (t[0] == "hgt") {
+                        passport.height_unit = std::string_view{std::end(t[1]) - 2, 2};
+                        passport.height = std::atoi(t[1].data());
+                    } else if (t[0] == "hcl") {
+                        passport.hair_colour = t[1];
+                    } else if (t[0] == "ecl") {
+                        passport.eye_colour = t[1];
+                    } else if (t[0] == "pid") {
+                        passport.passport_id = t[1];
+                    } else if (t[0] == "cid") {
+                        // We do not care
+                    } else {
+                        throw std::runtime_error("Invalid input");
+                    }
                 }
                 ++current_line;
             }
@@ -49,10 +79,9 @@ struct day_four {
     void part_one() const {
         const auto start = std::chrono::high_resolution_clock::now();
         int count{0};
+
         for (const auto& passport : input) {
-            const bool valid = passport.contains("byr") && passport.contains("iyr") && passport.contains("eyr") && passport.contains("hgt")
-                               && passport.contains("hcl") && passport.contains("ecl") && passport.contains("pid");
-            count += valid;
+            count += passport.hasAllFields();
         }
         const auto end = std::chrono::high_resolution_clock::now();
 
@@ -64,50 +93,44 @@ struct day_four {
         const auto start = std::chrono::high_resolution_clock::now();
         int count{0};
         for (const auto& passport : input) {
-            if (!(passport.contains("byr") && passport.contains("iyr") && passport.contains("eyr") && passport.contains("hgt") && passport.contains("hcl")
-                  && passport.contains("ecl") && passport.contains("pid"))) {
-                continue;
-            }
-            const auto byr = std::atoi(passport.at("byr").data());
-            if (byr < 1920 || byr > 2002) {
+            if (!passport.hasAllFields()) {
                 continue;
             }
 
-            const auto iyr = std::atoi(passport.at("iyr").data());
-            if (iyr < 2010 || iyr > 2020) {
+            if (passport.birth_year < 1920 || passport.birth_year > 2002) {
                 continue;
             }
 
-            const auto eyr = std::atoi(passport.at("eyr").data());
-            if (eyr < 2020 || eyr > 2030) {
+            if (passport.issue_year < 2010 || passport.issue_year > 2020) {
                 continue;
             }
 
-            const auto hgt = passport.at("hgt");
-            if (hgt.ends_with("in")) {
-                const auto hgt = std::atoi(passport.at("hgt").data());
-                if (hgt < 59 || hgt > 76) {
+            if (passport.expiration_year < 2020 || passport.expiration_year > 2030) {
+                continue;
+            }
+
+            if (passport.height_unit == "in") {
+                if (passport.height < 59 || passport.height > 76) {
                     continue;
                 }
-            } else if (hgt.ends_with("cm")) {
-                const auto hgt = std::atoi(passport.at("hgt").data());
-                if (hgt < 150 || hgt > 193) {
+            } else if (passport.height_unit == "cm") {
+                if (passport.height < 150 || passport.height > 193) {
                     continue;
                 }
             } else {
                 continue;
             }
 
-            if (const auto m = ctre::match<"#[0123456789abcdefABCDEF]{6}">(passport.at("hcl")); !m) {
+            if (const auto m = ctre::match<"#[0123456789abcdefABCDEF]{6}">(passport.hair_colour); !m) {
                 continue;
             }
 
-            const auto ecl = passport.at("ecl");
-            if (!(ecl == "amb" || ecl == "blu" || ecl == "brn" || ecl == "gry" || ecl == "grn" || ecl == "hzl" || ecl == "oth")) {
+            if (!(passport.eye_colour == "amb" || passport.eye_colour == "blu" || passport.eye_colour == "brn" || passport.eye_colour == "gry"
+                  || passport.eye_colour == "grn" || passport.eye_colour == "hzl" || passport.eye_colour == "oth")) {
                 continue;
             }
 
-            if (const auto m = ctre::match<"\\d{9}">(passport.at("pid")); !m) {
+            if (const auto m = ctre::match<"\\d{9}">(passport.passport_id); !m) {
                 continue;
             }
 
