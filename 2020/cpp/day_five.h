2d907fb9d514102a108fb62513807fc9eacdd0dd
Jamie Quigley <jamie@quigley.xyz>
2020-12-05 15:31:06 +0000

    Clean up day five code

--- 2020/cpp/day_five.h
+++ 2020/cpp/day_five.h
@@ -22,42 +22,6 @@ struct day_five {
     struct boarding_pass_t {
         const int row;
         const int col;
-        //        const std::string_view rowPath;
-        //        const std::string_view colPath;
-
-        //        inline int getRow() const {
-        //
-        //            int upper_limit{127};
-        //            int lower_limit{0};
-        //            for (const auto c : rowPath) {
-        //                switch (c) {
-        //                    case 'F':
-        //                        upper_limit = std::midpoint(upper_limit, lower_limit) - 1;
-        //                        break;
-        //                    case 'B':
-        //                        lower_limit = std::midpoint(upper_limit, lower_limit);
-        //                        break;
-        //                }
-        //            }
-        //
-        //            return lower_limit;
-        //        }
-        //
-        //        inline int getCol() const {
-        //            int upper_limit{7};
-        //            int lower_limit{0};
-        //            for (const auto c : colPath) {
-        //                switch (c) {
-        //                    case 'L':
-        //                        upper_limit = std::midpoint(upper_limit, lower_limit) - 1;
-        //                        break;
-        //                    case 'R':
-        //                        lower_limit = std::midpoint(upper_limit, lower_limit);
-        //                        break;
-        //                }
-        //            }
-        //            return lower_limit;
-        //        }
 
         inline int getSeatID() const {
             return (row * 8) + col;
@@ -66,13 +30,13 @@ struct day_five {
     std::vector<std::string> input_strings;
     std::vector<boarding_pass_t> input;
 
-    day_five() : input_strings{readFile("Inputs/day_five.txt")}{
+    day_five() : input_strings{readFile("Inputs/day_five.txt")} {
         const auto start = std::chrono::high_resolution_clock::now();
         for (auto& str : input_strings) {
-            std::replace_if(
-                std::begin(str), std::end(str), [](char c) { return c == 'F' || c == 'L'; }, '0');
-            std::replace_if(
-                std::begin(str), std::end(str), [](char c) { return c == 'B' || c == 'R'; }, '1');
+            std::ranges::replace_if(
+                str, [](char c) { return c == 'F' || c == 'L'; }, '0');
+            std::ranges::replace_if(
+                str, [](char c) { return c == 'B' || c == 'R'; }, '1');
 
             auto firstSeven = std::string_view(std::begin(str), std::begin(str) + 7);
             auto lastThree = std::string_view(std::end(str) - 3, std::end(str));
@@ -97,7 +61,7 @@ struct day_five {
 
     void part_two() const {
         const auto start = std::chrono::high_resolution_clock::now();
-        auto rng = input | std::ranges::views::transform([](const boarding_pass_t& boarding_pass) { return boarding_pass.getSeatID(); });
+        auto rng = std::ranges::views::transform(input, [](const boarding_pass_t& boarding_pass) { return boarding_pass.getSeatID(); });
         std::vector<int> ids{rng.begin(), rng.end()};
 
         std::ranges::sort(ids);
@@ -107,7 +71,7 @@ struct day_five {
 
         for (int i = lowest; i <= highest; ++i) {
             if (!std::ranges::binary_search(ids, i))
-                if (std::ranges::binary_search(ids, i-1) && std::ranges::binary_search(ids, i+1)) {
+                if (std::ranges::binary_search(ids, i - 1) && std::ranges::binary_search(ids, i + 1)) {
                     const auto end = std::chrono::high_resolution_clock::now();
                     fmt::print("The answer for day five part two is {}\n", i);
                     fmt::print("Took {}ns\n", std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count());
