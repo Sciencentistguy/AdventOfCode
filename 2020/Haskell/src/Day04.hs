4641e945cb7dd2f2f5d7c6229ca4aded88158f3d
Jamie Quigley <jamie@quigley.xyz>
2021-07-15 21:08:44 +0100

    Day 4

--- 2020/Haskell/src/Day04.hs
+++ 2020/Haskell/src/Day04.hs
@@ -4,15 +4,16 @@ module Day04
 where
 
 import Common
+import Data.Char
+import Data.List
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import Text.Read (readMaybe)
 
-requiredFields :: [Text.Text]
-requiredFields = map Text.pack ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
-validatePassport :: [String] -> Bool
-validatePassport (key : value : _) = case key of
+isValidPassport :: [String] -> Bool
+isValidPassport (key : value : _) = case key of
   "hgt" -> case unit of
     "cm" -> not $ height < 150 || height > 193
     "in" -> not $ height < 59 || height > 76
@@ -23,30 +24,32 @@ validatePassport (key : value : _) = case key of
   "byr" -> not $ valueAsInt < 1920 || valueAsInt > 2002
   "iyr" -> not $ valueAsInt < 2010 || valueAsInt > 2020
   "eyr" -> not $ valueAsInt < 2020 || valueAsInt > 2030
-  "hcl" ->
-    (length value == 7) && (head value == '#')
-      && all (`elem` "0123456789abcdef") (tail value)
-  "ecl" -> value `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-  "pid" -> (length value == 9) && all (`elem` "0123456789") value
+  "hcl" -> (length value == 7) && (head value == '#') && all isHexDigit (tail value)
+  "ecl" -> case value of
+    "amb" -> True
+    "blu" -> True
+    "brn" -> True
+    "gry" -> True
+    "grn" -> True
+    "hzl" -> True
+    "oth" -> True
+    _ -> False
+  "pid" -> (length value == 9) && all isDigit value
   "cid" -> True
   _ -> False
   where
     valueAsInt = read value
-validatePassport _ = error "unreachable"
+isValidPassport _ = unreachable ""
 
 day04 :: IO ()
 day04 = do
-  input_Text <- Text.lines <$> Text.readFile "/home/jamie/Git/AdventOfCode/2020/Inputs/day_04.txt"
-  let input_strs = map Text.unpack input_Text
+  input_strs <- lines <$> readFile "/home/jamie/Git/AdventOfCode/2020/Inputs/day_04.txt"
   -- part 1
   let grouped_entries = groupEntries input_strs
-  let valid_passports =
-        filter
-          (\passport -> all (\need -> Text.isInfixOf need (Text.pack passport)) requiredFields)
-          grouped_entries
+      valid_passports = filter (\x -> all (`isInfixOf` x) requiredFields) grouped_entries
   putStr "The answer for day four part one is "
   print $ length valid_passports
   -- part 2
+  let splitted = fmap (split ':') . split ' ' <$> valid_passports
   putStr "The answer for day four part two is "
-  let splitted = map (map (split ':') . split ' ') valid_passports
-  print $ length $ filter (all validatePassport) splitted
+  print $ length $ filter (all isValidPassport) splitted
