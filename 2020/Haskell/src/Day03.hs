84fcd19bfb8f40b2148903286f4a9dc725d19e4c
Jamie Quigley <jamie@quigley.xyz>
2021-07-15 14:20:28 +0100

    Day 03

--- 2020/Haskell/src/Day03.hs
+++ 2020/Haskell/src/Day03.hs
@@ -11,29 +11,28 @@ type Map = [String]
 isTree :: Map -> Int -> Int -> Bool
 isTree map x y = ((map !! y) !! x_mod_length) == '#'
   where
-    x_mod_length = x `mod` length (head map)
+    map_length = length $ head map
+    x_mod_length = x `mod` map_length
 
 getTreesSlope :: Map -> Int -> Int -> Int
 getTreesSlope worldMap xStep yStep = go worldMap xStep yStep 0 0 0
   where
+    yLimit = length worldMap
     go worldMap xStep yStep x y acc
       | y >= yLimit = acc
       | isTree worldMap x y = nextStep (acc + 1)
       | otherwise = nextStep acc
       where
         nextStep = go worldMap xStep yStep (x + xStep) (y + yStep)
-    yLimit = length worldMap
 
 day03 :: IO ()
 day03 = do
-  input_Text <- Text.lines <$> Text.readFile "/home/jamie/Git/AdventOfCode/2020/Inputs/day_03.txt"
-  let input_strs = map Text.unpack input_Text
+  input_strs <- lines <$> readFile "/home/jamie/Git/AdventOfCode/2020/Inputs/day_03.txt"
   -- part 1
   putStr "The answer for day three part one is "
   print $ getTreesSlope input_strs 3 1
   -- part 2
   putStr "The answer for day three part two is "
-  print $
-    product $
-      [getTreesSlope input_strs x 1 | x <- [1, 3 .. 7]]
-        ++ [getTreesSlope input_strs 1 2]
+  print $ product do
+    (x, y) <- [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    return $ getTreesSlope input_strs x y
