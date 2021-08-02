5c7ce11dcf5588cfb2de0c2d498f53cd0c58f95c
Jamie Quigley <jamie@quigley.xyz>
2021-08-02 18:29:58 +0100

    2020 Day 5 in Rust

---
+++ 2020/rust/src/day05.rs
@@ -0,0 +1,77 @@
+use std::time::{Duration, Instant};
+
+fn parse_input(input: &str) -> (Vec<(u8, u8)>, Duration) {
+    let start = Instant::now();
+    let res = input
+        .lines()
+        .map(|pass| {
+            let (row, seat) = pass.split_at(7);
+            let mut rowint = 0;
+            for (idx, val) in row.as_bytes().iter().rev().enumerate() {
+                let idx = 1u8 << idx;
+                if *val == b'B' {
+                    rowint |= idx
+                }
+            }
+            let mut seatint = 0;
+            for (idx, val) in seat.as_bytes().iter().rev().enumerate() {
+                let idx = 1u8 << idx;
+                if *val == b'R' {
+                    seatint |= idx
+                }
+            }
+            (rowint, seatint)
+        })
+        .collect();
+    let end = Instant::now();
+    (res, end - start)
+}
+
+fn solve_part1(input: &[(u8, u8)]) -> (usize, Duration) {
+    let start = Instant::now();
+    let res = input
+        .iter()
+        .map(|(row, col)| (*row as usize * 8) + *col as usize)
+        .max()
+        .unwrap();
+    let end = Instant::now();
+    (res, end - start)
+}
+
+fn solve_part2(input: &[(u8, u8)]) -> (usize, Duration) {
+    let start = Instant::now();
+    let mut ids = input
+        .iter()
+        .map(|(row, col)| (*row as usize * 8) + *col as usize)
+        .collect::<Vec<_>>();
+    ids.sort_unstable();
+    for id in 0usize.. {
+        if ids.binary_search(&id).is_err()
+            && ids.binary_search(&id.saturating_sub(1)).is_ok()
+            && ids.binary_search(&id.saturating_add(1)).is_ok()
+        {
+            let end = Instant::now();
+            return (id, end - start);
+        }
+    }
+    unreachable!()
+}
+
+pub fn run(input: String) {
+    let (parsed_input, time) = parse_input(&input);
+    println!("Day 05, parsing input took {}ns", time.as_nanos());
+    let (p1, time) = solve_part1(&parsed_input);
+    println!("Day 05, part 1: {}. Took {}ns", p1, time.as_nanos());
+    let (p2, time) = solve_part2(&parsed_input);
+    println!("Day 05, part 2: {}. Took {}ns", p2, time.as_nanos());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn day05_parse() {
+        let input = "FBFBBFFRLR";
+        assert_eq!(parse_input(input).0[0], (44, 5));
+    }
+}
--- 2020/rust/src/main.rs
+++ 2020/rust/src/main.rs
@@ -2,6 +2,7 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 fn open_input(day: u8) -> std::io::Result<String> {
     let mut path = std::env::current_dir()?;
@@ -17,5 +18,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     day02::run(open_input(2)?);
     day03::run(open_input(3)?);
     day04::run(open_input(4)?);
+    day05::run(open_input(5)?);
     Ok(())
 }
