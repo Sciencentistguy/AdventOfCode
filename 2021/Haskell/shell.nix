e0badc6fb81171387b5114286dd48a35d71e5d0b
Jamie Quigley <jamie@quigley.xyz>
2022-01-19 13:05:01 +0000

    Remove haskell.nix

--- 2021/Haskell/shell.nix
+++ 2021/Haskell/shell.nix
@@ -1,6 +1,5 @@
 let
-  # Pinned on 2021-11-04 at 01:48 UTC, to nixpkgs-unstable
-  #nixpkgs_url = "https://github.com/NixOS/nixpkgs/archive/550dab224a26ec25e20e82c0c8bfc764e01b772e.tar.gz";
+  # Pinned on 2022-01-19 to nixpkgs-unstable
   nixpkgs_url = "https://github.com/NixOS/nixpkgs/archive/e5a50e8f2995ff359a170d52cc40adbcfdd92ba4.tar.gz";
   # aliases in shellHooks don't work with zsh-nix-shell. See https://github.com/chisui/zsh-nix-shell/issues/6
   zdotdir_url = "https://gist.githubusercontent.com/chisui/bba90fccc930f614743dc259fbadae6d/raw/4108222addc1d646c1b0a6d12130083e2219ad28/zdotdir.nix";
