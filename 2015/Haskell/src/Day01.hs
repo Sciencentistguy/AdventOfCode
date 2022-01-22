4cd33c031d69851838d5b7b19a4b0d75117ef9a1
Jamie Quigley <jamie@quigley.xyz>
2022-01-22 19:46:42 +0000

    Simplify solution for 2015d1

--- 2015/Haskell/src/Day01.hs
+++ 2015/Haskell/src/Day01.hs
@@ -4,44 +4,32 @@ module Day01
 where
 
 import AOC
+import Control.Applicative (Applicative (liftA2))
+import Data.List (inits)
+import Data.List.Extra (elemIndex)
 import Data.Text (Text)
 import qualified Data.Text as Text
 
-type Parsed = [Direction]
+type Parsed = [Char]
 
-data Direction = Up | Down deriving (Show, Eq)
+numUps :: [Char] -> Int
+numUps = length . filter (== '(')
 
-pDirection :: Char -> Maybe Direction
-pDirection c = case c of
-  '(' -> return Up
-  ')' -> return Down
-  _ -> Nothing
+numDowns :: [Char] -> Int
+numDowns = length . filter (== ')')
 
-sumFloors :: Num p => [Direction] -> p
-sumFloors floors = go floors 0
-  where
-    go (x : xs) acc = case x of
-      Up -> go xs (acc + 1)
-      Down -> go xs (acc - 1)
-    go _ acc = acc
+destinationFloor :: [Char] -> Int
+destinationFloor = liftA2 (-) numUps numDowns
 
-trackFloors :: Int -> [Direction] -> Maybe Int
-trackFloors target floors = go (zip [0 ..] floors) target 0
-  where
-    go ((i, x) : xs) target acc =
-      if acc == target
-        then Just i
-        else case x of
-          Up -> go xs target (acc + 1)
-          Down -> go xs target (acc - 1)
-    go [] _ _ = Nothing
+trackFloors' :: Int -> [Char] -> Maybe Int
+trackFloors' target floors = elemIndex target $ destinationFloor <$> inits floors
 
 day01 :: Runner Parsed Int
 day01 =
   let year = 2015
       day = 1
       parser :: Text -> Maybe Parsed
-      parser input = traverse pDirection (Text.unpack input)
-      part1 = return . sumFloors
-      part2 = trackFloors (-1)
+      parser = return . Text.unpack
+      part1 = return . destinationFloor
+      part2 = trackFloors' (-1)
    in Runner {..}
