35c557943785fe151ce545d503dfb975f2b865c2
Jamie Quigley <jamie@quigley.xyz>
2022-01-22 22:18:26 +0000

    2015 Day 5 Part 1 in Haskell

--- 2015/Haskell/app/Main.hs
+++ 2015/Haskell/app/Main.hs
@@ -7,6 +7,7 @@ import qualified Data.Text as Text
 import Day01
 import Day02
 import Day03
+import Day05
 import System.Environment (lookupEnv)
 
 main :: IO ()
@@ -20,3 +21,4 @@ main = do
   runAoC token day01
   runAoC token day02
   runAoC token day03
+  runAoC token day05
---
+++ 2015/Haskell/src/Day05.hs
@@ -0,0 +1,46 @@
+module Day05
+  ( day05,
+  )
+where
+
+import AOC
+import Common
+import Control.Applicative (liftA3)
+import Data.Functor (($>))
+import Data.HashSet (HashSet)
+import qualified Data.HashSet as HS
+import Data.Hashable (Hashable)
+import Data.List
+import Data.Text (Text)
+import qualified Data.Text as Text
+
+type Parsed = [String]
+
+(&&&) :: Bool -> Bool -> Bool -> Bool
+(&&&) a b c = a && b && c
+
+isNice :: String -> Bool
+isNice = liftA3 (&&&) hasThreeVowels hasLetterPair hasNoBannedPairs
+
+hasLetterPair :: String -> Bool
+hasLetterPair s = any (`isInfixOf` s) letterPairs
+  where
+    letterPairs = [a : [a] | a <- ['a' .. 'z']]
+
+hasThreeVowels :: String -> Bool
+hasThreeVowels s = length (filter (`elem` "aeiou") s) >= 3
+
+hasNoBannedPairs :: String -> Bool
+hasNoBannedPairs s = not $ any (`isInfixOf` s) bannedPairs
+  where
+    bannedPairs = ["ab", "cd", "pq", "xy"]
+
+day05 :: Runner Parsed Int
+day05 =
+  let year = 2015
+      day = 5
+      parser :: Text -> Maybe Parsed
+      parser = return . fmap Text.unpack . Text.lines
+      part1 = return . length . filter isNice
+      part2 = undefined
+   in Runner {..}
