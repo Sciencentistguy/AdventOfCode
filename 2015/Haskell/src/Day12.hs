75665f3f696da6aa13d378b6bd078d27463b939c
Jamie Quigley <jamie@quigley.xyz>
2022-10-20 13:18:40 +0100

    2015 Day 12 in Haskell

--- 2015/Haskell/src/Day12.hs
+++ 2015/Haskell/src/Day12.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day12
   ( day12,
   )
@@ -6,28 +9,46 @@ where
 import AOC
 import Common
 import Control.Applicative
+import Data.Aeson
+import qualified Data.Aeson.KeyMap as KM
+import Data.ByteString.Builder (toLazyByteString)
+import qualified Data.ByteString.Lazy as BSL
 import Data.Char (isDigit)
+import Data.Function
 import Data.List (groupBy)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, mapMaybe)
+import Data.Scientific (Scientific)
+import qualified Data.Scientific as S
 import Data.Text (Text)
 import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
 import Safe
 
-type Parsed = String
+type Parsed = Value
 
-numbers :: String -> [Int]
-numbers s = catMaybes $ readMay <$> groupBy f s
-  where
-    f a b = g a && g b
-    g = liftA2 (||) (== '-') isDigit
+collectNums :: (Object -> Bool) -> Value -> Scientific
+collectNums shouldCollect = fix \go -> \case
+  Object o ->
+    if shouldCollect o
+      then sum $ go <$> o
+      else 0
+  Array a -> sum $ go <$> a
+  Number n -> n
+  _ -> 0
 
-day12 :: Runner Parsed Int
+day12 :: Runner Parsed Integer
 day12 =
   let year = 2015
       day = 12
       parser :: Text -> Maybe Parsed
-      parser = return . Text.unpack
-      part1 :: Parsed -> Maybe Int
-      part1 inpt = return $ sum $ numbers inpt
-      part2 = undefined
+      parser = decode' . BSL.fromStrict . Text.encodeUtf8
+      part1 :: Parsed -> Maybe Integer
+      part1 inpt = case S.floatingOrInteger $ collectNums (const True) inpt of
+        Left _ -> Nothing
+        Right i -> Just i
+      part2 inpt =
+        let shouldCollect v = not (KM.member "red" v) && ("red" `notElem` KM.elems v)
+         in case S.floatingOrInteger $ collectNums shouldCollect inpt of
+              Left _ -> Nothing
+              Right i -> Just i
    in Runner {..}
