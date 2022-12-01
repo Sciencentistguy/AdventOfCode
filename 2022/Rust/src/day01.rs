058cfa63de8533d48b41f64139fbae523e421712
Jamie Quigley <jamie@quigley.xyz>
2022-12-01 20:40:32 +0000

    Benchmarks for 2022 Day 01

--- 2022/Rust/src/day01.rs
+++ 2022/Rust/src/day01.rs
@@ -1,22 +1,22 @@
 use tap::Tap;
 
-fn parse(inpt: &str) -> Vec<u64> {
+pub fn parse(inpt: &str) -> Vec<u64> {
     inpt.split("\n\n")
         .map(|s| s.lines().map(|s| s.parse::<u64>().unwrap()).sum())
         .collect::<Vec<_>>()
         .tap_mut(|x| x.sort_unstable())
 }
 
-fn part1(parsed: &[u64]) -> u64 {
+pub fn part1(parsed: &[u64]) -> u64 {
     *parsed.last().unwrap()
 }
 
-fn part2(parsed: &[u64]) -> u64 {
+pub fn part2(parsed: &[u64]) -> u64 {
     parsed.iter().rev().take(3).sum()
 }
 
-pub fn run(input: String) {
-    let parsed = parse(&input);
+pub fn run(input: &str) {
+    let parsed = parse(input);
     println!("Part 1: {}", part1(&parsed));
     println!("Part 2: {}", part2(&parsed));
 }
--- 2022/Rust/src/main.rs
+++ 2022/Rust/src/main.rs
@@ -3,15 +3,13 @@ use std::{error::Error, process::ExitCode};
 use clap::Parser;
 use emergence::AoC;
 
-mod day01;
-
 fn main() -> Result<ExitCode, Box<dyn Error>> {
     let args = Opt::parse();
 
     let aoc = AoC::new(2022)?;
 
     match args.day {
-        1 => day01::run(aoc.read_or_fetch(1)?),
+        1 => aoc_2022::day01::run(&aoc.read_or_fetch(1)?),
 
         day => {
             eprintln!("Day {day} does not exist / is not implemented yet");
