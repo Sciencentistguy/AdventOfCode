a8cbb066c2208ba86418abf8091c960839cdc83f
Jamie Quigley <jamie@quigley.xyz>
2022-12-01 18:49:03 +0000

    Add day argument to 2022/Rust

--- 2022/Rust/src/main.rs
+++ 2022/Rust/src/main.rs
@@ -1,13 +1,28 @@
-use std::error::Error;
+use std::{error::Error, process::ExitCode};
 
+use clap::Parser;
 use emergence::AoC;
 
 mod day01;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<ExitCode, Box<dyn Error>> {
+    let args = Opt::parse();
+
     let aoc = AoC::new(2022)?;
 
-    day01::run(aoc.read_or_fetch(1)?);
+    match args.day {
+        1 => day01::run(aoc.read_or_fetch(1)?),
 
-    Ok(())
+        day => {
+            eprintln!("Day {day} does not exist / is not implemented yet");
+            return Ok(ExitCode::FAILURE);
+        }
+    }
+
+    Ok(ExitCode::SUCCESS)
+}
+
+#[derive(Parser)]
+struct Opt {
+    day: usize,
 }
