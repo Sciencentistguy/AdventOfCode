679f587b20ad4c298959a28cfa2bc132e57dfc20
Jamie Quigley <jamie@quigley.xyz>
2022-12-04 16:17:56 +0000

    Optimise 2022 Day 03 in Rust

--- 2022/Rust/benches/day03.rs
+++ 2022/Rust/benches/day03.rs
@@ -4,10 +4,18 @@ use criterion::{criterion_group, criterion_main, Criterion};
 pub fn criterion_benchmark(c: &mut Criterion) {
     let aoc = emergence::AoC::new(2022).unwrap();
 
-    let input = aoc.read_or_fetch(3).unwrap();
-    let parsed = day03::parse(&input);
+    let mut input = aoc.read_or_fetch(3).unwrap();
 
-    c.bench_function("day03::parse", |b| b.iter(|| day03::parse(&input)));
+    c.bench_function("day03::parse", move |b| {
+        b.iter(|| {
+            let _ = day03::parse(&mut input);
+        })
+    });
+
+    let mut input = aoc.read_or_fetch(3).unwrap();
+
+    let parsed = day03::parse(&mut input).to_owned();
+
     c.bench_function("day03::part1", |b| b.iter(|| day03::part1(&parsed)));
     c.bench_function("day03::part2", |b| b.iter(|| day03::part2(&parsed)));
 }
--- 2022/Rust/src/day03.rs
+++ 2022/Rust/src/day03.rs
@@ -1,7 +1,24 @@
 use itertools::Itertools;
 
-pub fn parse(input: &str) -> Vec<&str> {
-    input.lines().collect()
+pub fn parse(input: &mut str) -> Vec<(&str, &str)> {
+    assert!(input.is_ascii());
+    // Safety: input is ascii, moving bytes around is Fine™
+    unsafe {
+        for line in input.as_bytes_mut().split_mut(|&x| x == b'\n') {
+            let mid = line.len() / 2;
+            let (left, right) = line.split_at_mut(mid);
+            left.sort_unstable();
+            right.sort_unstable();
+        }
+    }
+
+    input
+        .lines()
+        .map(|line| {
+            let mid = line.len() / 2;
+            line.split_at(mid)
+        })
+        .collect()
 }
 
 fn priority(x: u8) -> usize {
@@ -12,17 +29,13 @@ fn priority(x: u8) -> usize {
     }
 }
 
-pub fn part1(parsed: &[&str]) -> usize {
+pub fn part1(parsed: &[(&str, &str)]) -> usize {
     parsed
         .iter()
-        .map(|line| {
-            let mid = line.len() / 2;
-            line.split_at(mid)
-        })
         .map(|(first, second)| {
             first
                 .bytes()
-                .filter(|c| second.as_bytes().contains(c))
+                .filter(|c| second.as_bytes().binary_search(c).is_ok())
                 .unique()
                 .map(priority)
                 .sum::<usize>()
@@ -30,14 +43,27 @@ pub fn part1(parsed: &[&str]) -> usize {
         .sum()
 }
 
-pub fn part2(parsed: &[&str]) -> usize {
+pub fn part2(parsed: &[(&str, &str)]) -> usize {
     parsed
         .iter()
         .array_chunks::<3>()
         .map(|[a, b, c]| {
             priority(
-                a.bytes()
-                    .find(|chr| b.contains(*chr as char) && c.contains(*chr as char))
+                a.0.bytes()
+                    .find(|chr| {
+                        (b.0.as_bytes().binary_search(chr).is_ok()
+                            || b.1.as_bytes().binary_search(chr).is_ok())
+                            && (c.0.as_bytes().binary_search(chr).is_ok()
+                                || c.1.as_bytes().binary_search(chr).is_ok())
+                    })
+                    .or_else(|| {
+                        a.1.bytes().find(|chr| {
+                            (b.0.as_bytes().binary_search(chr).is_ok()
+                                || b.1.as_bytes().binary_search(chr).is_ok())
+                                && (c.0.as_bytes().binary_search(chr).is_ok()
+                                    || c.1.as_bytes().binary_search(chr).is_ok())
+                        })
+                    })
                     .unwrap(),
             )
         })
@@ -45,7 +71,8 @@ pub fn part2(parsed: &[&str]) -> usize {
 }
 
 pub fn run(input: &str) {
-    let parsed = parse(input);
+    let mut input = input.to_owned();
+    let parsed = parse(&mut input);
     println!("Part 1: {}", part1(&parsed));
     println!("Part 2: {}", part2(&parsed));
 }
@@ -64,13 +91,15 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
     #[test]
     fn test_part1() {
-        let parsed = parse(INPUT);
+        let mut input = INPUT.to_owned();
+        let parsed = parse(&mut input);
         assert_eq!(157, part1(&parsed));
     }
 
     #[test]
     fn test_part2() {
-        let parsed = parse(INPUT);
+        let mut input = INPUT.to_owned();
+        let parsed = parse(&mut input);
         assert_eq!(70, part2(&parsed));
     }
 }
