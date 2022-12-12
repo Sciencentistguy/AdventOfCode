c2d5a31d74330701f82614e8f1bff52c59fc596a
Jamie Quigley <jamie@quigley.xyz>
2022-12-12 22:48:29 +0000

    Optimise 2022 Day 12
    I was being an dumb
    
    Thanks @Xychic

--- 2022/Rust/src/day12.rs
+++ 2022/Rust/src/day12.rs
@@ -1,35 +1,13 @@
-use arrayvec::ArrayVec;
-use pathfinding::prelude::*;
+use std::collections::{HashSet, VecDeque};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point2D {
-    x: i32,
-    y: i32,
-}
-
-impl Point2D {
-    fn successors(&self, grid: &[Vec<i32>]) -> ArrayVec<Point2D, 4> {
-        let &Point2D { x, y } = self;
-
-        let directions = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
-        let current = grid[y as usize][x as usize];
-
-        directions
-            .into_iter()
-            .filter(|d| {
-                d.0 >= 0 && d.1 >= 0 && d.0 < grid[0].len() as i32 && d.1 < grid.len() as i32
-            })
-            .filter(|direction| grid[direction.1 as usize][direction.0 as usize] <= current + 1)
-            .map(|direction| Point2D {
-                x: direction.0,
-                y: direction.1,
-            })
-            .collect()
-    }
+    x: usize,
+    y: usize,
 }
 
 pub struct Parsed {
-    map: Vec<Vec<i32>>,
+    map: Vec<Vec<usize>>,
     start: Point2D,
     end: Point2D,
 }
@@ -69,41 +47,61 @@ pub fn parse(inpt: &str) -> Parsed {
     }
 }
 
-fn distance(map: &[Vec<i32>], start: Point2D, end: Point2D) -> Option<usize> {
-    bfs(&start, |p| p.successors(map), |p| *p == end).map(|x| x.len() - 1)
+fn search(queue: &mut VecDeque<(Point2D, usize)>, end: Point2D, map: &[Vec<usize>]) -> usize {
+    let row_len = map[0].len();
+    let col_len = map.len();
+
+    let mut seen = HashSet::with_capacity(map.len() * map[0].len());
+    while let Some((p @ Point2D { x, y }, distance)) = queue.pop_front() {
+        if seen.contains(&p) {
+            continue;
+        }
+        seen.insert(p);
+
+        if p == end {
+            return distance;
+        }
+
+        let height = map[y][x];
+
+        for (dx, dy) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
+            let new_x = (x as isize + dx) as usize;
+            let new_y = (y as isize + dy) as usize;
+
+            if new_x < row_len && new_y < col_len {
+                let new_height = map[new_y][new_x];
+                if new_height <= height + 1 {
+                    queue.push_back((Point2D { x: new_x, y: new_y }, distance + 1))
+                }
+            }
+        }
+    }
+    unreachable!()
 }
 
 pub fn part1(parsed: &Parsed) -> usize {
-    distance(&parsed.map, parsed.start, parsed.end).unwrap()
-    // bfs(
-    // &parsed.start,
-    // |p| p.successors(&parsed.map),
-    // |p| *p == parsed.end,
-    // )
-    // .unwrap()
-    // .len()
-    // - 1
+    let mut queue = VecDeque::new();
+    queue.push_back((parsed.start, 0));
+    search(&mut queue, parsed.end, &parsed.map)
 }
 
 pub fn part2(parsed: &Parsed) -> usize {
-    let mut start_positions = Vec::new();
+    let mut start_positions = VecDeque::new();
 
     for y in 0..parsed.map.len() {
         for x in 0..parsed.map[y].len() {
             if parsed.map[y][x] == 0 {
-                start_positions.push(Point2D {
-                    x: x as _,
-                    y: y as _,
-                });
+                start_positions.push_back((
+                    Point2D {
+                        x: x as _,
+                        y: y as _,
+                    },
+                    0,
+                ));
             }
         }
     }
-
-    start_positions
-        .into_iter()
-        .filter_map(|start| distance(&parsed.map, start, parsed.end))
-        .min()
-        .unwrap()
+    search(&mut start_positions, parsed.end, &parsed.map)
 }
 
 pub fn run(input: &str) {
