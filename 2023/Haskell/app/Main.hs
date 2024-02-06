63215d5822eee3a86dff6e09451df38006652aca
Jamie Quigley <jamie@quigley.xyz>
2024-02-06 20:02:15 +0000

    2023 Day 04 in Haskell

--- 2023/Haskell/app/Main.hs
+++ 2023/Haskell/app/Main.hs
@@ -3,21 +3,23 @@ module Main where
 import AoC
 import Day01
 import Day02
+import Day04
 import Safe
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let day = case args of
-        [x] -> readMay x :: Maybe Int
-        _ -> Nothing
-  token <- getToken
-  case day of
-    Nothing -> putStrLn "Please provide a day number as an argument."
-    Just 1 -> runAoC token day01
-    Just 2 -> runAoC token day02
-    Just x -> do
-      putStr "Day '" 
-      putStr $ show x
-      putStrLn "' not implemented yet."
+    args <- getArgs
+    let day = case args of
+            [x] -> readMay x :: Maybe Int
+            _ -> Nothing
+    token <- getToken
+    case day of
+        Nothing -> putStrLn "Please provide a day number as an argument."
+        Just 1 -> runAoC token day01
+        Just 2 -> runAoC token day02
+        Just 4 -> runAoC token day04
+        Just x -> do
+            putStr "Day '"
+            putStr $ show x
+            putStrLn "' not implemented yet."
---
+++ 2023/Haskell/src/Day04.hs
@@ -0,0 +1,48 @@
+module Day04 (day04) where
+
+import AoC
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Text.Read (readMaybe)
+
+type Parsed = [Card]
+
+data Card = Card
+    { winningNumbers :: [Int]
+    , numbers :: [Int]
+    }
+
+pCard :: Text -> Maybe Card
+pCard line = do
+    let line' = Text.concat $ tail $ Text.split (== ':') line
+    [winningNumbers, numbers] <-
+        traverse (traverse (readMaybe . Text.unpack) . Text.words) $ Text.split (== '|') line'
+    return Card{..}
+
+matches :: Card -> [Int]
+matches Card{..} = filter (`elem` winningNumbers) numbers
+
+day04 :: Runner Parsed Int
+day04 =
+    let year = 2023
+        day = 4
+        parser :: Text -> Maybe Parsed
+        parser = traverse pCard . Text.lines
+        part1 :: Parsed -> Maybe Int
+        part1 =
+            let value [] = 0
+                value x = 2 ^ (length x - 1)
+             in return . sum . fmap (value . matches)
+        part2 :: Parsed -> Maybe Int
+        part2 =
+            let
+                f :: (Int, Card) -> Map Int Int -> Map Int Int
+                f (n, c) acc = M.insert n value acc
+                  where
+                    numMatches = length $ matches c
+                    value = succ $ sum $ take numMatches $ M.elems acc
+             in
+                return . sum . M.elems . foldr f mempty . zip [1 ..]
+     in Runner{..}
