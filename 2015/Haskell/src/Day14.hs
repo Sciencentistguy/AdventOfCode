4cdb9173d9b82c77f3bcff2f7766baf6cc619b5c
Jamie Quigley <jamie@quigley.xyz>
2024-11-13 16:42:59 +0000

    Lensify 2015-04

--- 2015/Haskell/src/Day14.hs
+++ 2015/Haskell/src/Day14.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Day14
   ( day14,
   )
@@ -5,7 +7,9 @@ where
 
 import AoC
 import AoC.Common (modifyAtN, runNTimes)
+import Control.Lens
 import Data.List.Extra (maximumOn)
+import Data.Maybe (fromJust)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Safe
@@ -13,50 +17,62 @@ import Safe
 type Parsed = [Reindeer]
 
 data Reindeer = Reindeer
-  { speed :: Int,
-    flyTime :: Int,
-    restTime :: Int
+  { _speed :: Int,
+    _flyTime :: Int,
+    _restTime :: Int
   }
   deriving (Show)
 
 pLine :: Text -> Maybe Reindeer
 pLine text = do
   [_, _, _, speed', _, _, flyTime', _, _, _, _, _, _, restTime', _] <- return $ Text.unpack <$> Text.split (== ' ') text
-  speed <- readMay speed'
-  flyTime <- readMay flyTime'
-  restTime <- readMay restTime'
+  _speed <- readMay speed'
+  _flyTime <- readMay flyTime'
+  _restTime <- readMay restTime'
   return Reindeer {..}
 
 data Mode = Flying | Resting deriving (Show)
 
+toggleMode :: Mode -> Mode
+toggleMode Flying = Resting
+toggleMode Resting = Flying
+
 data State = State
-  { deer :: Reindeer,
-    distance :: Int,
-    stateTime :: Int,
-    state :: Mode,
-    points :: Int
+  { _deer :: Reindeer,
+    _distance :: Int,
+    _stateTime :: Int,
+    _state :: Mode,
+    _points :: Int
   }
   deriving (Show)
+
+makeLenses ''Reindeer
+makeLenses ''State
 
 mkInitialState :: Reindeer -> State
 mkInitialState deer = State deer 0 0 Flying 0
 
 step :: State -> State
-step st@State {..} = case state of
+step st = case st ^. state of
   Flying ->
-    if stateTime >= flyTime deer
-      then st {stateTime = 1, state = Resting}
-      else st {distance = distance + speed deer, stateTime = stateTime + 1}
+    if st ^. stateTime >= st ^. deer . flyTime
+      then st & flipState . resetStateTime
+      else st & flyForward . wait1
   Resting ->
-    if stateTime >= restTime deer
-      then st {stateTime = 1, distance = distance + speed deer, state = Flying}
-      else st {stateTime = stateTime + 1}
+    if st ^. stateTime >= st ^. deer . restTime
+      then st & flipState . flyForward . resetStateTime
+      else st & wait1
+  where
+    wait1 = stateTime %~ (+ 1)
+    flyForward st = st & distance %~ (+ (st ^. deer . speed))
+    flipState = state %~ toggleMode
+    resetStateTime = stateTime .~ 1
 
 stepAll :: [State] -> [State]
 stepAll states =
   let allStepped = step <$> states
-      inLeadIdx = fst $ maximumOn (distance . snd) $ zip [0 :: Int ..] allStepped
-   in modifyAtN (\st@State {..} -> st {points = points + 1}) inLeadIdx allStepped
+      maxDistance = fromJust $ maximumOf (traversed . distance) allStepped
+   in allStepped & traversed . filtered (\s -> s ^. distance == maxDistance) . points %~ (+ 1)
 
 race :: [Reindeer] -> [State]
 race deers =
@@ -69,6 +85,6 @@ day14 =
       day = 14
       parser :: Text -> Maybe Parsed
       parser = traverse pLine . Text.lines
-      part1 = return . distance . maximumOn distance . race
-      part2 = return . points . maximumOn points . race
+      part1 = return . (^. distance) . maximumOn (^. distance) . race
+      part2 = return . (^. points) . maximumOn (^. points) . race
    in Runner {..}
