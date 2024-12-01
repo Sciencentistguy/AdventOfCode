11b1ff1a3bc095bda71843343dce4f5a070e2a27
Jamie Quigley <jamie@quigley.xyz>
2024-12-01 15:35:44 +0000

    Tweak 2024 Day 01

--- 2024/Rust/src/day01.rs
+++ 2024/Rust/src/day01.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+
+use atoi::FromRadix10;
+
+
 type Parsed = (Vec<u64>, Vec<u64>);
 type Solution = u64;
 
@@ -9,16 +14,17 @@ pub fn parse(input: &str) -> Parsed {
     let mut right = Vec::with_capacity(length);
 
     for line in lines {
-        let (a, b) = line.split_at(line.find(' ').unwrap());
-        let a = a.parse::<u64>().unwrap();
-        let b = b.trim().parse::<u64>().unwrap();
+        let line = line.as_bytes();
+
+        let (a, idx) = u64::from_radix_10(line);
+        let (b, _) = u64::from_radix_10(&line[idx + 3..]);
 
         left.push(a);
         right.push(b);
     }
 
-    left.sort();
-    right.sort();
+    left.sort_unstable();
+    right.sort_unstable();
 
     (left, right)
 }
@@ -30,20 +36,20 @@ pub fn part1((left, right): &Parsed) -> Solution {
         .sum()
 }
 
-fn count_in(v: &[u64], n: u64) -> u64 {
-    let mut count = 0;
+fn count(v: &[u64]) -> HashMap<u64, u64> {
+    let mut counts = HashMap::with_capacity(v.len());
     for &num in v {
-        if num == n {
-            count += 1;
-        }
+        *counts.entry(num).or_insert(0) += 1;
     }
-    count
+    counts
 }
 
 pub fn part2((left, right): &Parsed) -> Solution {
+    let counts = count(right);
+
     let mut counter = 0;
     for &num in left {
-        counter += num * count_in(right, num);
+        counter += num * counts.get(&num).unwrap_or(&0);
     }
     counter
 }
@@ -52,4 +58,25 @@ pub fn run(input: &str) {
     let parsed = parse(input);
     println!("Part 1: {}", part1(&parsed));
     println!("Part 2: {}", part2(&parsed));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&parse(INPUT)), 11);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&parse(INPUT)), 31);
+    }
 }
