971e628d05dd0e1982e67d6f62e332e8a9a98ebd
Jamie Quigley <jamie@quigley.xyz>
2024-12-01 10:46:35 +0000

    2024 Day 01 in Rust

--- 2024/Rust/src/day01.rs
+++ 2024/Rust/src/day01.rs
@@ -1,18 +1,51 @@
-use crate::Result;
-
-type Parsed = ();
+type Parsed = (Vec<u64>, Vec<u64>);
 type Solution = u64;
 
 pub fn parse(input: &str) -> Parsed {
-    todo!();
+    let lines = input.lines().collect::<Vec<_>>();
+    let length = lines.len();
+
+    let mut left = Vec::with_capacity(length);
+    let mut right = Vec::with_capacity(length);
+
+    for line in lines {
+        let (a, b) = line.split_at(line.find(' ').unwrap());
+        let a = a.parse::<u64>().unwrap();
+        let b = b.trim().parse::<u64>().unwrap();
+
+        left.push(a);
+        right.push(b);
+    }
+
+    left.sort();
+    right.sort();
+
+    (left, right)
 }
 
-pub fn part1(parsed: &Parsed) -> Solution {
-    todo!();
+pub fn part1((left, right): &Parsed) -> Solution {
+    left.iter()
+        .zip(right.iter())
+        .map(|(a, &b)| a.abs_diff(b))
+        .sum()
 }
 
-pub fn part2(parsed: &Parsed) -> Solution {
-    todo!();
+fn count_in(v: &[u64], n: u64) -> u64 {
+    let mut count = 0;
+    for &num in v {
+        if num == n {
+            count += 1;
+        }
+    }
+    count
+}
+
+pub fn part2((left, right): &Parsed) -> Solution {
+    let mut counter = 0;
+    for &num in left {
+        counter += num * count_in(right, num);
+    }
+    counter
 }
 
 pub fn run(input: &str) {
--- 2024/Rust/src/lib.rs
+++ 2024/Rust/src/lib.rs
@@ -1,10 +1,3 @@
-#![feature(array_chunks)]
-#![feature(array_windows)]
-#![feature(inline_const)]
-#![feature(pattern)]
-#![feature(never_type)]
-#![feature(let_chains)]
-
 type Result<T> = color_eyre::Result<T>;
 
 pub mod day01;
