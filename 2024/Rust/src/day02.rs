9eb7fd90692a839a6f21ae3ae4d8df7f8f735e87
Jamie Quigley <jamie@quigley.xyz>
2024-12-03 21:26:49 +0000

    Optimise 2024 Day 02

--- 2024/Rust/src/day02.rs
+++ 2024/Rust/src/day02.rs
@@ -1,13 +1,13 @@
 use std::ops::Index;
 
-type Parsed = Vec<Vec<u64>>;
+type Parsed = TwoDimensionalVec<u64>;
 type Solution = usize;
 
 #[derive(Clone, Copy)]
-struct TwoPartSlice<'a>(&'a [u64], &'a [u64]);
+struct TwoPartSlice<'a, T>(&'a [T], &'a [T]);
 
-impl Index<usize> for TwoPartSlice<'_> {
-    type Output = u64;
+impl<T> Index<usize> for TwoPartSlice<'_, T> {
+    type Output = T;
     fn index(&self, index: usize) -> &Self::Output {
         if index < self.0.len() {
             &self.0[index]
@@ -17,26 +17,67 @@ impl Index<usize> for TwoPartSlice<'_> {
     }
 }
 
-impl TwoPartSlice<'_> {
+impl<T> TwoPartSlice<'_, T> {
     fn len(&self) -> usize {
         self.0.len() + self.1.len()
     }
 }
 
-pub fn parse(input: &str) -> Parsed {
-    input
-        .lines()
-        .map(|x| {
-            x.split_ascii_whitespace()
-                .map(|x| x.parse().unwrap())
-                .collect()
+pub struct TwoDimensionalVec<T> {
+    storage: Vec<T>,
+    row_lengths: Vec<usize>,
+}
+
+impl<T> TwoDimensionalVec<T> {
+    fn new() -> Self {
+        Self {
+            storage: Vec::new(),
+            row_lengths: Vec::new(),
+        }
+    }
+
+    #[allow(dead_code)] // used in test
+    fn push_row(&mut self, row: &[T])
+    where
+        T: Clone,
+    {
+        self.row_lengths.push(row.len());
+        self.storage.extend_from_slice(row);
+    }
+
+    fn extend_row(&mut self, row: impl Iterator<Item = T>)
+    where
+        T: Clone,
+    {
+        self.row_lengths.push(0);
+
+        for item in row {
+            self.storage.push(item);
+            *self.row_lengths.last_mut().unwrap() += 1;
+        }
+    }
+
+    fn row_iter(&self) -> impl Iterator<Item = &[T]> {
+        self.row_lengths.iter().scan(0, |start, &len| {
+            let end = *start + len;
+            let slice = &self.storage[*start..end];
+            *start = end;
+            Some(slice)
         })
-        .collect()
+    }
+}
+
+pub fn parse(input: &str) -> Parsed {
+    let mut v = TwoDimensionalVec::new();
+    for line in input.lines() {
+        v.extend_row(line.split_ascii_whitespace().map(|x| x.parse().unwrap()));
+    }
+    v
 }
 
 pub fn part1(parsed: &Parsed) -> Solution {
     let mut count = 0;
-    for report in parsed {
+    for report in parsed.row_iter() {
         let mut desc = true;
         let mut inc = true;
         let mut gradual = true;
@@ -54,7 +95,7 @@ pub fn part1(parsed: &Parsed) -> Solution {
     count
 }
 
-fn tolerate(v: &[u64]) -> impl Iterator<Item = TwoPartSlice> {
+fn tolerate(v: &[u64]) -> impl Iterator<Item = TwoPartSlice<u64>> {
     (0..v.len()).map(move |i| {
         let (a, b) = v.split_at(i);
         TwoPartSlice(a, &b[1..])
@@ -63,7 +104,7 @@ fn tolerate(v: &[u64]) -> impl Iterator<Item = TwoPartSlice> {
 
 pub fn part2(parsed: &Parsed) -> Solution {
     let mut count = 0;
-    for report in parsed {
+    for report in parsed.row_iter() {
         for report in tolerate(report) {
             let mut desc = true;
             let mut inc = true;
@@ -112,5 +153,19 @@ mod tests {
     #[test]
     fn test_part2() {
         assert_eq!(part2(&parse(INPUT)), P2_SOLUTION);
+    }
+
+    #[test]
+    fn test_two_dimensional_vec() {
+        let mut v = TwoDimensionalVec::new();
+        v.push_row(&[1, 2, 3]);
+        v.push_row(&[4, 5, 6, 7]);
+        v.push_row(&[8, 9]);
+
+        let mut it = v.row_iter();
+        assert_eq!(it.next(), Some(&[1, 2, 3] as _));
+        assert_eq!(it.next(), Some(&[4, 5, 6, 7] as _));
+        assert_eq!(it.next(), Some(&[8, 9] as _));
+        assert_eq!(it.next(), None);
     }
 }
